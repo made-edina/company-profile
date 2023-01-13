c74c3ee78fdcdebbd45273274d4b0b5cb1d979d0
made-edina <118876413+made-edina@users.noreply.github.com>
2023-01-13 12:14:03 +0800

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
                     <div class="col-lg-3">
                         <img className="logo-white" src="../logo-white.png" alt="logo"/>
                     </div>
-                    <div class="col-lg-6 address order-lg-last">
+                    <div class="col-lg-5 address order-lg-last">
                         <Form.Select aria-label="Default select example">
                         <option>Technology Department</option>
                         <option value="1">Technology Department</option>
@@ -17,7 +17,7 @@ function Footer() {
                         </Form.Select>
                         <h3>Jl. Lembong No 8 Kel. Braga Kec. Sumur Bandung, Kota Bandung, Jawa Barat</h3>
                     </div>
-                    <div class="col-lg-3 d-flex justify-content-lg-center">
+                    <div class="col-lg-4 d-flex justify-content-lg-center">
                         <ul>
                             <li><a href="#whoweare">Who We Are</a> </li>
                             <li><a href="#corevalue">Our Values</a> </li>
